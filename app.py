1693f09e5a67740351d19de8974ab85a405b50f7
Ilham Ramadhani <ilham10ihsangmail.com@Ilhams-MacBook-Air.local>
2026-03-27 02:33:13 +0700

    prepare render deploy

--- app.py
+++ app.py
@@ -3503,5 +3503,8 @@ def get_stok_for_pemesanan():
             'data': []
         }), 500
 
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5002, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- start.sh
+++ start.sh
@@ -1,18 +1,5 @@
-set -eu
+#!/usr/bin/env bash
 
-export PYTHONUNBUFFERED=true
+pip install -r requirements.txt
 
-VIRTUALENV=.data/venv
-
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-$VIRTUALENV/bin/python3 app.py
-Footer
+gunicorn app:app --bind 0.0.0.0:$PORT
